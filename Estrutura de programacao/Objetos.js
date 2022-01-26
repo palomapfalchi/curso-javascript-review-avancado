d9b219020249083fe48e82e5594201e4dc6129c1
Paloma Falchi <palomapfalchi@gmail.com>
2022-01-26 14:04:09 -0300

    objetos add

--- Estrutura de programacao/Objetos.js
+++ Estrutura de programacao/Objetos.js
@@ -15,5 +15,34 @@ pessoa.nome = "Kimera"
 pessoa.jogos = [...pessoa.jogos, "Cryptocars"];
 pessoa.idade = 20
 delete pessoa.peso 
+pessoa.falar = function(texto){
+    pessoa.nome = "Alexa";
+    console.log(`${pessoa.nome} disse: ${texto}`);
+}
 
 console.log("🚀 ~ file: Objetos.js ~ line 10 ~", pessoa)
+pessoa.falar("Olá, tudo bem?"); 
+
+//DESESTRUTURAÇÃO
+
+const carro = {
+    marca: "Hyundai",
+    modelo: "Elantra",
+    placa: "ADS-9999"
+}
+
+let {marca, modelo} = carro
+console.log("🚀 ~ file: Objetos.js ~ line 35 ~ marca, modelo", marca, modelo)
+
+let objA = {
+    nome: "Daniel"
+}
+
+objB = objA;
+
+let objC = {
+    nome: "daniel"
+}
+
+console.log("🚀 ~ file: Objetos.js ~ line 42 ~ objB = objA", objA == objB)
+console.log("🚀 ~ file: Objetos.js ~ line 42 ~ objB = objA", objA == objC)
