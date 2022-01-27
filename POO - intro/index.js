5d02654602842b33d58550b0392ada058b5ab51f
Paloma Falchi <palomapfalchi@gmail.com>
2022-01-27 17:48:42 -0300

    exercicios de POO

---
+++ POO - intro/index.js
@@ -0,0 +1,27 @@
+function Person(first, last, age, gender, interests) {
+    this.name = {
+      'first': first,
+      'last' : last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function() {
+        if (this.gender == "male") {
+            console.log((this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.'));
+        } else {
+            console.log((this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.'));
+        }
+    };
+    this.greeting = function() {
+      console.log(('Hi! I\'m ' + this.name.first + '.'));
+    };
+}
+  
+
+  var person1 = new Person("Baby", "Smith", 32, "female", ["music", "skiing", "movie"]);
+
+  console.log(person1["age"]);
+  console.log(person1.interests[1]);
+  console.log(person1.bio());
+  console.log(person1.greeting());
